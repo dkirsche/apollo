c41b9b667ac4339958f4a8758c193300ec50b554
Dmitri Skjorshammer <dmitriskj@gmail.com>
2021-05-07 13:39:27 -0700

    Simplify App loading.

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -4,28 +4,14 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "./index.css";
 import App from "./App";
 
-import { ThemeSwitcherProvider } from "react-css-theme-switcher";
-
-const themes = {
-  dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
-  light: `${process.env.PUBLIC_URL}/light-theme.css`,
-};
-
-const prevTheme = window.localStorage.getItem("theme");
-
-let subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract"
-
 const client = new ApolloClient({
-  // uri: subgraphUri,
   uri: "https://api.thegraph.com/subgraphs/name/dkirsche/asset-price-history",
   cache: new InMemoryCache()
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme ? prevTheme : "light"}>
-      <App subgraphUri={subgraphUri}/>
-    </ThemeSwitcherProvider>
+    <App />
   </ApolloProvider>,
   document.getElementById("root"),
 );
